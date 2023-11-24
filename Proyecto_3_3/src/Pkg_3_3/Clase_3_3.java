968fc11d3bcc6f7c3b5e5c62fbab73078cd21e28
AlbertoCeu <abernardez@ceu.es>
2023-11-24 07:36:17 +0100

    Primera Version

---
+++ Proyecto_3_3/src/Pkg_3_3/Clase_3_3.java
@@ -0,0 +1,10 @@
+package Pkg_3_3;
+
+public class Clase_3_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Practica 3 3 Alberto Bernardez");
+	}
+
+}
